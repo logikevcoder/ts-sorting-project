4d167eba17c74335f9da014787a313dec758478f
Kevin Napier <kevin.napier@rotary.org>
2020-07-31 12:59:53 -0500

    added LinkedList sorting functionality and example output

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Sorter } from './Sorter';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(23);
+linkedList.add(2424);
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 11, 0, 200]);
 const charactersCollection = new CharactersCollection('XaAgseaFfFggyBkJ');
 
+const linkedListSorter = new Sorter(linkedList);
 const sorter = new Sorter(numbersCollection);
 const charSorter = new Sorter(charactersCollection);
 
+linkedListSorter.sort();
+linkedList.print();
 sorter.sort();
 charSorter.sort();
 
